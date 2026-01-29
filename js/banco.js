e87a25bb71a731736b50cb2e285e46b2a7428794
CLedsonB <edzzon2@gmail.com>
2026-01-29 17:21:12 -0300

    alteracao para imagens web, adicao de mangas e detalhes, exibicao de detalhes dos mangas

--- js/banco.js
+++ js/banco.js
@@ -2,93 +2,173 @@ var json = {
   mangas : [
   {
     "titulo" : "Dr. Stone",
-    "imagem" : "./img/ds.jpeg",
+    "imagem" : "./img/ds.webp",
     "autor" : "Riichiro Inagaki",
     "genero" : ["shounen","aventura"],
     "tipo" : "mangá",
-    "concluido" : false,
-    "capitulos" : 80,
-    "sinopse" : "lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom"
+    "concluido" : "Não",
+    "capitulos" : 26,
+    "sinopse" : "Dr. Stone é sobre o gênio científico Senku Ishigami que, após toda a humanidade ser misteriosamente petrificada por 3.700 anos, acorda em um mundo primitivo e usa seu vasto conhecimento para reconstruir a civilização do zero, usando a ciência como sua maior ferramenta para recriar tudo, desde medicamentos até tecnologia moderna, junto de seu amigo forte, Taiju, e outros aliados, enfrentando desafios naturais e humanos para restaurar o progresso."
   },
   {
     "titulo" : "One Piece",
-    "imagem" : "./img/op.jpg",
+    "imagem" : "./img/op.webp",
     "autor" : "Eiichiro Oda",
     "genero" : ["shounen","colegial","aventura"],
     "tipo" : "mangá",
-    "concluido" : false,
-    "capitulos" : 80,
-    "sinopse" : "lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom"
+    "concluido" : "Não",
+    "capitulos" : 1100,
+    "sinopse" : "One Piece segue as aventuras de Monkey D. Luffy, um jovem que ganha propriedades elásticas após comer uma 'fruta do diabo' e sonha em se tornar o Rei dos Piratas. Ao lado da tripulação dos Chapéus de Palha, ele navega por mares perigosos em busca do lendário tesouro, o One Piece, escondido pelo ex-rei Gold D. Roger. "
   },
   {
     "titulo" : "Blue Exorcist",
-    "imagem" : "./img/blueExor.jpg",
+    "imagem" : "./img/blueExor.webp",
     "autor" : "Katou Kazue",
     "genero" : ["shounen","colegial","aventura"],
     "tipo" : "mangá",
-    "concluido" : false,
-    "capitulos" : 80,
-    "sinopse" : "lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom"
+    "concluido" : "Sim",
+    "capitulos" : 150,
+    "sinopse" : "Um adolescente Rin Okumura que descobre ser filho de Satanás e herdar seus poderes demoníacos, jurando vingar a morte de seu pai adotivo, um exorcista, tornando-se um exorcista para combater as forças do mal na Academia Vera Cruz, lidando com a dualidade de sua herança e sua luta entre o bem e o mal, o mundo humano e o mundo dos demônios."
   },
   {
     "titulo" : "Death Note",
-    "imagem" : "./img/deathNote.jpeg",
+    "imagem" : "./img/deathNote.webp",
     "autor" : "Tsugumi Ohba",
     "genero" : ["shounen","colegial","aventura"],
     "tipo" : "mangá",
-    "concluido" : false,
-    "capitulos" : 80,
-    "sinopse" : "lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom"
+    "concluido" : "Sim",
+    "capitulos" : 108,
+    "sinopse" : " Light Yagami, um gênio que encontra um caderno sobrenatural capaz de matar qualquer um cujo nome seja escrito nele, decidindo usá-lo para eliminar criminosos e criar um mundo 'perfeito', mas logo se torna o alvo do brilhante detetive L, iniciando um intenso jogo de gato e rato com implicações morais e psicológicas profundas. "
   },
   {
     "titulo" : "gintama",
-    "imagem" : "./img/gintama.jpg",
+    "imagem" : "./img/gintama.webp",
     "autor" : "Hideaki Sorachi",
     "genero" : ["shounen","colegial","aventura"],
     "tipo" : "mangá",
-    "concluido" : false,
-    "capitulos" : 80,
-    "sinopse" : "lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom"
+    "concluido" : "Sim",
+    "capitulos" : 704,
+    "sinopse" : "Gintama se passa em um Japão feudal alternativo (Edo) que foi invadido e conquistado por alienígenas tecnologicamente avançados conhecidos como Amanto. Sob o novo regime, os samurais foram desarmados e a posse de espadas foi proibida, transformando os outrora orgulhosos guerreiros em cidadãos de segunda classe. "
   },
   {
-    "titulo" : "jojo parte 4",
-    "imagem" : "./img/joj.jpg",
+    "titulo" : "jojo aventuras bizarras",
+    "imagem" : "./img/jojo.webp",
     "autor" : "Hirohiko Araki",
     "genero" : ["shounen","colegial","aventura"],
     "tipo" : "mangá",
-    "concluido" : false,
-    "capitulos" : 80,
-    "sinopse" : "lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom"
+    "concluido" : "Não",
+    "capitulos" : 131,
+    "sinopse" : "A trama atravessa gerações, começando na Inglaterra vitoriana e expandindo-se pelo mundo (e pelo tempo). Cada parte foca em um novo protagonista da linhagem Joestar que sempre apelidado de JoJo e que deve enfrentar forças sobrenaturais, desde vampiros ancestrais até usuários de poderes psíquicos. O conflito principal nasce da rivalidade entre Jonathan Joestar e seu irmão adotivo, Dio Brando, cujas ações ecoam e amaldiçoam a família por mais de um século."
   },
   {
     "titulo" : "one puch man",
-    "imagem" : "./img/onp.jpg",
+    "imagem" : "./img/onp.webp",
     "autor" : "One",
     "genero" : ["shounen","colegial","aventura"],
     "tipo" : "mangá",
-    "concluido" : false,
-    "capitulos" : 80,
-    "sinopse" : "lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom"
+    "concluido" : "Não",
+    "capitulos" : 167,
+    "sinopse" : "A história gira em torno de Saitama, um homem comum que, após um treinamento rigoroso (e estranhamente simples), tornou-se o herói mais forte do mundo."
   },
   {
     "titulo" : "Steins Gate",
-    "imagem" : "./img/steinsGate.jpg",
+    "imagem" : "./img/steinsGate.webp",
     "autor" : "Jukki Hanada",
     "genero" : ["shounen","colegial","aventura"],
     "tipo" : "mangá",
-    "concluido" : false,
-    "capitulos" : 80,
-    "sinopse" : "lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom"
+    "concluido" : "Sim",
+    "capitulos" : 14,
+    "sinopse" : "A história se passa em 2010, no distrito de Akihabara, em Tóquio. Rintaro Okabe é um jovem universitário excêntrico que se autointitula um 'cientista louco' sob o pseudônimo Hououin Kyouma. Ele fundou o 'Laboratório de Gadgets do Futuro' em um apartamento alugado, onde passa o tempo criando invenções inúteis com seus amigos: a doce Mayuri Shiina e o hacker talentoso Itaru 'Daru' Hashida."
   },
   {
     "titulo" : "Super Onze",
-    "imagem" : "./img/sz.jpg",
+    "imagem" : "./img/sz.webp",
     "autor" : "Tenya Yabuno",
     "genero" : ["shounen","colegial","aventura"],
     "tipo" : "mangá",
-    "concluido" : false,
-    "capitulos" : 80,
-    "sinopse" : "lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom, lorem ypsom"
+    "concluido" : "Sim",
+    "capitulos" : 45,
+    "sinopse" : "A história acompanha Satoru Endo (Mamoru Endo), um goleiro extremamente talentoso e neto de um dos maiores jogadores do Japão. O problema? Ele estuda na Escola Raimon, onde o clube de futebol está à beira da extinção, com apenas sete membros desmotivados. A reviravolta acontece quando a escola é desafiada pelo prestigiado Instituto Imperial. Para salvar o clube, Endo precisa recrutar novos jogadores e convencer o misterioso atacante de elite, Shuya Goenji, a voltar a jogar."
+  },
+    {
+    "titulo" : "Avatar : A Lenda de Aang",
+    "imagem" : "./img/avatar-aang.webp",
+    "autor" : ["Michael Dante", "Bryan Konietzko"],
+    "genero" : ["shounen","colegial","aventura"],
+    "tipo" : "mangá",
+    "concluido" : "Sim",
+    "capitulos" : 18,
+    "sinopse" : "O mundo é dividido em quatro nações, cada uma vinculada a um elemento: Tribo da Água, Reino da Terra, Nação do Fogo e Nômades do Ar. Algumas pessoas nessas nações têm a habilidade de manipular seu elemento nativo — a 'dobra'. O equilíbrio entre essas nações é mantido pelo Avatar, o único ser capaz de dominar os quatro elementos e o ponte entre o mundo físico e o espiritual."
+  },
+    {
+    "titulo" : "Blue Dragon",
+    "imagem" : "./img/blue-dragon.webp",
+    "autor" : "Akira Itou ",
+    "genero" : ["shounen","colegial","aventura"],
+    "tipo" : "mangá",
+    "concluido" : "Sim",
+    "capitulos" : 49,
+    "sinopse" : "A história se passa em um mundo onde as sombras das pessoas podem se manifestar como monstros poderosos. O protagonista é Shu, um garoto impulsivo e determinado que vive em uma pequena vila. Tudo muda quando sua vila é atacada pelas forças do maligno Nene, um tirano tecnológico que busca dominar o mundo. Durante o conflito, Shu desperta sua sombra: o lendário e temperamental Blue Dragon."
+  },
+    {
+    "titulo" : "Os Cavaleiros dos Zodíaco",
+    "imagem" : "./img/cdz.webp",
+    "autor" : "Masami Kurumada",
+    "genero" : ["shounen","colegial","aventura"],
+    "tipo" : "mangá",
+    "concluido" : "Sim",
+    "capitulos" : 234,
+    "sinopse" : "A história acompanha um grupo de jovens guerreiros chamados Cavaleiros, que juraram proteger a reencarnação da deusa grega Athena (Saori Kido) em sua missão de manter a paz na Terra contra as forças do mal. Para lutar, os Cavaleiros utilizam sua energia interior, o Cosmo, e vestem poderosas Armaduras sagradas que são inspiradas nas constelações do céu."
+  },
+    {
+    "titulo" : "Akame-ga Kill",
+    "imagem" : "./img/akame.webp",
+    "autor" : "Takahiro",
+    "genero" : ["shounen","colegial","aventura"],
+    "tipo" : "mangá",
+    "concluido" : "Sim",
+    "capitulos" : 78,
+    "sinopse" : "A história acompanha Tatsumi, um jovem lutador ingênuo que parte de sua pequena vila em direção à Capital Imperial para ganhar dinheiro e salvar seu povo da pobreza. No entanto, ao chegar lá, ele descobre que a cidade reluzente é, na verdade, um poço de corrupção, sadismo e injustiça. Após presenciar os horrores cometidos pela elite da capital, Tatsumi é recrutado pela Night Raid, um grupo de assassinos de elite que faz parte do Exército Revolucionário."
+  },
+    {
+    "titulo" : "Komi Can't Communicate'",
+    "imagem" : "./img/komisan.webp",
+    "autor" : "Tomohito Desu",
+    "genero" : ["shounen","colegial","aventura"],
+    "tipo" : "mangá",
+    "concluido" : "Não",
+    "capitulos" : 113,
+    "sinopse" : "Komi Can't Communicate (Komi não consegue se comunicar) é sobre a linda e popular estudante Shouko Komi, que sofre de um grave transtorno de ansiedade social, impedindo-a de falar e se comunicar, embora deseje fazer amigos; com a ajuda de seu colega de classe normal, Hitohito Tadano, ela estabelece a meta de fazer 100 amigos para superar sua condição e interagir com o mundo. A série acompanha suas tentativas engraçadas e comoventes de socializar, explorando o mal-entendido das pessoas que a veem como metida, quando na verdade ela luta para se expressar"
+  },
+    {
+    "titulo" : "Nanatsu no Taizai",
+    "imagem" : "./img/nanatsu.webp",
+    "autor" : "Nakaba Suzuki",
+    "genero" : ["shounen","colegial","aventura"],
+    "tipo" : "mangá",
+    "concluido" : "Sim",
+    "capitulos" : 345,
+    "sinopse" : "Nanatsu no Taizai acompanha a jornada da Princesa Elizabeth para salvar seu reino, que foi dominado por um golpe de estado dos tirânicos Cavaleiros Sagrados.  Para retomar a paz em Liones, ela parte em busca dos Sete Pecados Capitais, um grupo de guerreiros lendários e poderosos que foram injustamente acusados de tentar derrubar o reino há dez anos e, desde então, vivem escondidos."
+  },
+    {
+    "titulo" : "Kakegurui",
+    "imagem" : "./img/kakegurui.webp",
+    "autor" : "Homura Kawamoto",
+    "genero" : ["shounen","colegial","aventura"],
+    "tipo" : "mangá",
+    "concluido" : "Não",
+    "capitulos" : 100,
+    "sinopse" : "A rotina da escola é abalada com a transferência de Yumeko Jabami. À primeira vista, ela parece uma estudante doce e ingênua, mas logo revela ser uma apostadora compulsiva. Diferente dos outros alunos, que jogam por dinheiro ou status, Yumeko joga pelo puro prazer do risco. Ela não tem medo de perder tudo, o que a torna uma ameaça imprevisível para o Grêmio Estudantil, que controla a escola com mão de ferro."
+  },
+    {
+    "titulo" : "Zatch Bell",
+    "imagem" : "./img/zatbell.webp",
+    "autor" : "Makoto Raibow",
+    "genero" : ["shounen","colegial","aventura"],
+    "tipo" : "mangá",
+    "concluido" : "Sim",
+    "capitulos" : 232,
+    "sinopse" : "A história acompanha Kiyo Takamine, um estudante japonês de 14 anos extremamente inteligente, mas socialmente isolado e entediado com o mundo. Sua vida muda drasticamente quando seu pai lhe envia um presente inusitado vindo da Inglaterra: um garotinho loiro chamado Zatch Bell. Zatch não é uma criança comum; ele é um Mamodo, um ser de outro mundo com poderes sobrenaturais. A cada mil anos, 100 crianças Mamodo são enviadas à Terra para lutarem entre si. O último sobrevivente se tornará o Rei do Mundo Mamodo."
   },
   ]
 };
--- js/exibirManga.js
+++ js/exibirManga.js
@@ -1,53 +1,131 @@
-window.onload = (Event) => {
+// Função para exibir os mangás
+function exibirMangas(mangas) {
+  const caixaConteudo = document.querySelector('.displayManga');
+  caixaConteudo.innerHTML = "";
+  
+  const meuh3 = document.createElement('h3');
+  meuh3.classList.add('tituloAgrupamento');
+  meuh3.textContent = 'Últimos Lançamentos';
+  
+  caixaConteudo.appendChild(meuh3);
+  
 
-    var manga = json['mangas']
-    preencherDiv(manga);
-    // nomearId(manga);
+  mangas.forEach((manga) => {
+    const meuDiv = document.createElement('div');
+    meuDiv.classList.add('manga');
+    meuDiv.id = manga.titulo.replace(/ /g, '_');
 
-    const listaManga = document.querySelectorAll('.manga');
-  };
+    const meuA1 = document.createElement('a');
+    meuA1.href = '#';
+    meuA1.addEventListener('click', () => exibirDetalhesManga(manga));
 
-function preencherDiv(manga) {
-    var caixaConteudo = document.querySelector('.displayManga');
-    
-    for (var i = 0; i <= manga.length; i++) {
-        const meuDiv = document.createElement('div');
-        meuDiv.classList.toggle('manga');
-        const titulo = manga[i].titulo
-        meuDiv.id = titulo.replace(/ /g, '_');
+    const meuImg = document.createElement('img');
+    meuImg.classList.add('capa');
+    meuImg.src = manga.imagem;
+    meuImg.alt = manga.titulo;
 
-        const meuA1 = document.createElement('a');
-        const meuA2 = document.createElement('a');
-        var meuImg = document.createElement('img');
-        meuImg.classList.toggle('capa');
-        var meuH3 = document.createElement('h3');
-        meuH3.classList.toggle('titulo');
-        var meuP = document.createElement('p');
-        meuP.classList.toggle('capitulo');
+    const meuA2 = document.createElement('a');
+    meuA2.href = '#';
+    meuA2.addEventListener('click', () => exibirDetalhesManga(manga));
 
-        meuImg.src = manga[i].imagem;
-        meuImg.alt = manga[i].titulo;
-        meuImg.placeholder = manga[i].titulo;
-        meuH3.textContent = manga[i].titulo;
-        meuP.textContent = "Capitulo " + manga[i].capitulos;
+    const meuH3 = document.createElement('h3');
+    meuH3.classList.add('titulo');
+    meuH3.textContent = manga.titulo;
 
-        caixaConteudo.appendChild(meuDiv);
-        meuDiv.appendChild(meuA1);
-        meuA1.appendChild(meuImg);
-        meuDiv.appendChild(meuA2);
-        meuA2.appendChild(meuH3);
-        meuDiv.appendChild(meuP);
+    const meuP = document.createElement('p');
+    meuP.classList.add('capitulo');
+    meuP.textContent = "Capitulo " + manga.capitulos;
 
-        var id = document.getElementsByClassName('manga')[i].id;
-        meuA1.href = 'infoManga.html?manga=' + id;
-        meuA2.href = 'infoManga.html?manga=' + id;
-      }
+    caixaConteudo.appendChild(meuDiv);
+    meuDiv.appendChild(meuA1);
+    meuA1.appendChild(meuImg);
+    meuDiv.appendChild(meuA2);
+    meuA2.appendChild(meuH3);
+    meuDiv.appendChild(meuP);
+  });
 }
 
-// function nomearId(manga) {
-//   for(var i = 0; i <= manga.length; i++) {
-//     var id = document.getElementsByClassName('manga')[i].id;
-//     meuA1.href = 'infoManga.html?manga=' + id;
-//     meuA2.href = 'infoManga.html?manga=' + id;
-//   }
-// }
+// Função para exibir os detalhes do manga
+function exibirDetalhesManga(manga) {
+  const caixaConteudo = document.querySelector('.displayManga');
+  caixaConteudo.innerHTML = "";
+
+  const meuLink = document.createElement('a');
+  meuLink.href = '#';
+  meuLink.classList.add('seta-voltar')
+  meuLink.textContent = '<-';
+  meuLink.addEventListener('click', () => {
+    const mangas = json['mangas'];
+    exibirMangas(mangas);
+  });
+  caixaConteudo.appendChild(meuLink);
+
+  const meuDiv1 = document.createElement('div');
+  meuDiv1.classList.add('bordaConteudo');
+
+  const meuDiv2 = document.createElement('div');
+  meuDiv2.classList.add('conterImagem');
+
+  const meuImg = document.createElement('img');
+  meuImg.classList.add('capaManga');
+  meuImg.src = manga.imagem;
+  meuImg.alt = "Capa do " + manga.titulo;
+
+  const meuP1 = document.createElement('p');
+  meuP1.classList.add('infoManga');
+  meuP1.textContent = "Titulo : ";
+  const meuB1 = document.createElement('b');
+  meuB1.textContent = manga.titulo;
+  meuP1.appendChild(meuB1);
+
+  const meuP2 = document.createElement('p');
+  meuP2.classList.add('infoManga');
+  meuP2.textContent = "Autor : ";
+  const meuB2 = document.createElement('b');
+  meuB2.textContent = manga.autor;
+  meuP2.appendChild(meuB2);
+
+  const meuP3 = document.createElement('p');
+  meuP3.classList.add('infoManga');
+  meuP3.textContent = "Gênero : ";
+  const meuB3 = document.createElement('b');
+  meuB3.textContent = manga.genero;
+  meuP3.appendChild(meuB3);
+
+  const meuP4 = document.createElement('p');
+  meuP4.classList.add('infoManga');
+  meuP4.textContent = "Capítulos : ";
+  const meuB4 = document.createElement('b');
+  meuB4.textContent = manga.capitulos;
+  meuP4.appendChild(meuB4);
+
+  const meuP5 = document.createElement('p');
+  meuP5.classList.add('infoManga');
+  meuP5.textContent = "Concluído : ";
+  const meuB5 = document.createElement('b');
+  meuB5.textContent = manga.concluido;
+  meuP5.appendChild(meuB5);
+  
+  const meuP6 = document.createElement('p');
+  meuP6.classList.add('infoManga');
+  meuP6.textContent = "Sinopse : ";
+  const meuB6 = document.createElement('b');
+  meuB6.textContent = manga.sinopse;
+  meuP6.appendChild(meuB6);
+  
+  
+  caixaConteudo.appendChild(meuDiv1);
+  meuDiv1.appendChild(meuDiv2);
+  meuDiv2.appendChild(meuImg);
+  meuDiv1.appendChild(meuP1);
+  meuDiv1.appendChild(meuP2);
+  meuDiv1.appendChild(meuP3);
+  meuDiv1.appendChild(meuP4);
+  meuDiv1.appendChild(meuP5);
+  meuDiv1.appendChild(meuP6);
+}
+
+window.onload = () => {
+  const mangas = json['mangas'];
+  exibirMangas(mangas);
+};
